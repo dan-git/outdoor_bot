3a3b4427e0da598d1ceb997bf35c1ccd456395fa
Jenny Barry <jbarry@csail.mit.edu>
2015-05-31 14:44:54 -0400

    Obstacle detector that looks for an obstacle within a cone

---
+++ include/navigation/ObstacleDetector.h
@@ -0,0 +1,33 @@
+#ifndef __OUTDOOR_BOT_NAV_OBSTACLE_DETECTOR_H__
+#define __OUTDOOR_BOT_NAV_OBSTACLE_DETECTOR_H__
+
+#include <boost/thread/mutex.hpp>
+
+#include <ros/ros.h>
+#include "sensor_msgs/LaserScan.h"
+
+namespace OutdoorBot
+{
+namespace Navigation
+{
+
+class ObstacleDetector
+{
+ public:
+  ObstacleDetector();
+
+  bool obstacleInCone(double cone_height, double cone_radius, double angle) const;
+
+ private:
+  void laserCallback(const sensor_msgs::LaserScan msg);
+
+  mutable boost::mutex lock_;
+  ros::Subscriber laserSub_;
+  sensor_msgs::LaserScan lastMsg_;
+  int min_obstacle_points_;
+};
+
+}  // namespace Navigation
+}  // namespace OutdoorBot
+
+#endif  // __OUTDOOR_BOT_NAV_OBSTACLE_DETECTOR_H__
